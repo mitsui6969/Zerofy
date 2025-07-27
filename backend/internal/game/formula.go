3496bed414caead02b54147266cde480f9dd61a5
takuma <lemonenergy.2061.7.29@gmail.com>
2025-07-28 03:15:39 +0900

    式の生成の実装をした。

---
+++ backend/internal/game/formula.go
@@ -0,0 +1,38 @@
+package game
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func CreateFormula() (string, int) {
+	//式を生成する
+	operators := []string{"+", "-", "×", "÷"}
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	randomNumber1 := r.Intn(10) + 1
+	randomNumber2 := r.Intn(10) + 1
+
+	operator := operators[r.Intn(len(operators))]
+
+	Question := fmt.Sprintf("%d %s %d", randomNumber1, operator, randomNumber2)
+
+	//生成された式の答えを計算
+	Answer := 0
+	switch operator {
+	case "+":
+		Answer = randomNumber1 + randomNumber2
+	case "-":
+		Answer = randomNumber1 - randomNumber2
+	case "×":
+		Answer = randomNumber1 * randomNumber2
+	case "÷":
+		// 0除算が起こらない前提
+		Answer = randomNumber1 / randomNumber2
+	}
+
+	return Question, Answer
+}
