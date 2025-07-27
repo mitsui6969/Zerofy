a5dcdfadbe479fb08e9083bfc01ff65eb1475921
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-07-27 22:25:55 +0900

    ファイル追加

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -32,7 +32,6 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <p>ここに表示したい</p>
 
       <Link href={"/start"}><button>Start へ</button></Link>
       <button onClick={() => sendToBack()}>hello backend!</button>
