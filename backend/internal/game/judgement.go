2a812edaeb498538657a320f16af0cf89315ff1c
hayao <mihokubota@outlook.jp>
2025-07-29 10:27:42 +0900

    judgementの実装

--- backend/internal/game/judgement.go
+++ backend/internal/game/judgement.go
@@ -1,1 +1,23 @@
 package game
+
+import "sync"
+
+var (
+	winner string
+	mu     sync.Mutex
+)
+
+// 正解の変数をCorrectAnswerにしています
+func Judgement(ID string, answer int, CorrectAnswer int) string {
+	// 排他的処理
+	mu.Lock()
+	defer mu.Unlock()
+
+	// 正誤判定と一番最初かどうか
+	if winner == "" && answer == CorrectAnswer {
+		winner = ID
+	}
+
+	//勝者を返す。正解者がいなかったら""になる
+	return winner
+}
