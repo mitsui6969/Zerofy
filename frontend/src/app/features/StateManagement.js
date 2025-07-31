f619eb3560c9dccf0ab9477a5ccc6e83ef1b15d9
hayao <mihokubota@outlook.jp>
2025-08-01 07:38:50 +0900

    フロントエンドの状態管理（修正版）

--- frontend/src/app/features/StateManagement.js
+++ frontend/src/app/features/StateManagement.js
@@ -1,9 +1,9 @@
-class StateManagement {
+class StatePointManagement {
 
     ID; // プレイヤーID
     point; // プレイヤーのポイント
-    answer; // プレイヤーの直近の回答
-    isAnswer = false; // プレイヤーの回答が適切かどうか
+    Bet; // プレイヤーの直近の回答
+    CanBet = false; // プレイヤーの回答が適切かどうか
 
     // コンストラクタ
     constructor(ID, point) {
@@ -12,13 +12,15 @@ class StateManagement {
     }
 
     // 回答の記録
-    Answered(answer) {
+    SetBet(Bet) {
         // 全角数字だったら半角にして、いずれも数字として扱えるようにする
-        this.answer = Number(answer.replace(/[０-９]/g, s => 
+        this.Bet = Number(Bet.replace(/[０-９]/g, s => 
             String.fromCharCode(s.charCodeAt(0) - 0xFEE0)));
 
-        // 何らかの理由で変換できないときfalseを返す
-        this.isAnswer = !(Number.isNaN(this.answer));
+        this.Bet = (this.Bet < 1 || 9 < this.Bet || this.Bet % 1 !== 0) ? NaN : this.Bet;
+
+        // 要件にあっていないときfalseをかえす
+        this.CanBet = !(Number.isNaN(this.Bet));
     }
 
     // ポイントの更新
@@ -30,7 +32,35 @@ class StateManagement {
     StatePrint() {
         console.log(`ID      : ${this.ID}`);
         console.log(`point   : ${this.point}`);
-        console.log(`answer  : ${this.answer}`);
-        console.log(`isAnswer: ${this.isAnswer}`);
+        console.log(`Bet     : ${this.Bet}`);
+        console.log(`CanBet  : ${this.CanBet}`);
+    }
+}
+class RoundStateManagement {
+    myself;
+    enemy;
+
+    constructor(myself, enemy) {
+        if (myself instanceof StatePointManagement && enemy instanceof StatePointManagement) {
+            this.myself = myself;
+            this.enemy = enemy;
+        } else {
+            throw new Error("Not Correctly instances");
+        }
+    }
+
+    UpdatePoint(myPoint, enPoint) {
+        this.myself.point = myPoint;
+        this.enemy.point  = enPoint;
+    }
+
+    SetBet(myBet, enBet) {
+        this.myself.SetBet(myBet);
+        this.enemy.SetBet(enBet);
+    }
+
+    Roundinfo() {
+        this.myself.StatePrint();
+        this.enemy.StatePrint();
     }
 }
