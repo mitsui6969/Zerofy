ea5ed68c6cb67d16309696ecdc4e27dba233daa8
haruya <kuroiwaharuya@gmail.com>
2025-08-02 14:05:37 +0900

    スペースキーを押すとそのまま入力できるようにした

--- frontend/src/app/components/Game/QuestionPhase.jsx
+++ frontend/src/app/components/Game/QuestionPhase.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function QuestionPhase({ data, ws }) {
-    const { expression } = data;
+    const [isStarted, setIsStarted] = useState(false);
+    const { expression } = "1+1"; // 仮のデータをここに入れる
+    // const { expression } = data; // 実際のデータを使用する場合こちらを有効にする
     const [answer, setAnswer] = useState('');
+    const inputRef = useRef(null);
 
-    const handleSubmit = () => {
+    useEffect(() => {
+    const handleStartGame = (e) => {
+        if (!isStarted && e.key === ' ') {
+            e.preventDefault();
+            setIsStarted(true);
+        }
+    };
+
+    document.addEventListener('keydown', handleStartGame);
+
+    if (isStarted && inputRef.current) {
+            inputRef.current.focus();
+        }
+
+    return () => {
+        document.removeEventListener('keydown', handleStartGame);
+    };
+    }, [isStarted]);
+
+        const handleSubmit = () => {
         ws.send(JSON.stringify({
         type: 'ANSWER',
         payload: {
@@ -12,16 +34,29 @@ export default function QuestionPhase({ data, ws }) {
             timeAt: new Date().toISOString(),
         },
         }));
+        setAnswer(''); 
+        };
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit();
+        }
     };
 
     return (
         <div>
         <h2 className="text-xl font-bold mb-4">計算式に答えてください！</h2>
-        <p className="text-lg mb-4">{expression} = ?</p>
+        {isStarted ? (
+            <p className="text-lg mb-4">{expression} = ?</p>
+        ) : (  
+            <p>スペースキーで準備完了！<br />Enterで解答を送信できます！</p>
+
+        )}
         <input
-            type="number"
+            type="text"
             value={answer}
             onChange={(e) => setAnswer(e.target.value)}
+            onKeyDown={handleKeyDown}
+            ref={inputRef}
             className="border p-2 mr-2"
         />
         <button
