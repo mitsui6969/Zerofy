4e633106d23af188fecead1432117ceccfe957b8
hayao <mihokubota@outlook.jp>
2025-08-03 14:19:26 +0900

    ゲーム結果の表示

--- frontend/src/app/components/Game/EndPhase.jsx
+++ frontend/src/app/components/Game/EndPhase.jsx
@@ -2,9 +2,54 @@ import React from 'react';
 
 export default function EndPhase() {
     //aaa//
+    const roundData = [
+        { はなこ: 20, たろう: 20 },
+        { はなこ: 16, たろう: 20 },
+        { はなこ: 10, たろう: 20 },
+        { はなこ: 10, たろう: 16 },
+        { はなこ: 10, たろう: 15 },
+        { はなこ: 5, たろう: 15 },
+        { はなこ: 0, たろう: 15 },
+    ];
+
+    const renderTable = () => {
+        const keys = Object.keys(roundData[0]);
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>ラウンド</th>
+                        {keys.map((key) => (
+                            <th key={key}>{key}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {roundData.map((round, index) => (
+                        <tr key={index}>
+                            <td>{index}</td>
+                            {keys.map((key) => (
+                                <td key={key}>{round[key]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    };
+
     return (
-        <div>
-        <p>EndPhase</p>
+        <div id="score-container">
+            <h2>Zerofy RESULT</h2>
+            <div id="table-wrapper">
+                {renderTable()}
+                <div id="button-container">
+                    <a href="/" className="corner-button">
+                        Zerofyページへ →
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
