bb780de51bd6099ba23420e25c11edb409f44cb2
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-08-05 17:18:52 +0900

    css反映

--- frontend/src/app/components/Game/QuestionPhase.jsx
+++ frontend/src/app/components/Game/QuestionPhase.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSocketStore } from '../../store/socketStore';
 import { usePlayerStore } from '../../features/payer/playerStore';
+import '../../style/Question.css';
 
 export default function QuestionPhase() {
     const { socket, isConnected, readyPlayers, formula, currentPoints, resetReadyState, resetIncorrectState, phase, isIncorrect } = useSocketStore();
